dd3c0680bb7410ea27407192fec94797378faa2b
DisperDev <slyfa34@gmail.com>
2020-04-22 13:36:11 +0400

    Добавил примечание о лицензии в файлы

--- src/main/java/knoblul/eosvstubot/frontend/login/LoginHolderEditDialog.java
+++ src/main/java/knoblul/eosvstubot/frontend/login/LoginHolderEditDialog.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package knoblul.eosvstubot.frontend.login;
 
 import knoblul.eosvstubot.backend.login.LoginHolder;
@@ -9,8 +22,9 @@ import java.awt.*;
 import java.io.IOException;
 
 /**
+ * <br><br>Module: eos-vstu-bot
+ * <br>Created: 22.04.2020 10:47
  * @author Knoblul
- * Created: 22.04.2020 10:47
  */
 public class LoginHolderEditDialog extends JComponent {
 	private JTextField usernameField;
